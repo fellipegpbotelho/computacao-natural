7280124cb0fb8df77f5780a58dc9503f20887847
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2019-03-26 09:06:54 -0300

    feature(Conversões): adiciona funções na home

--- main.m
+++ main.m
@@ -7,8 +7,20 @@ xMax = 5;
 tamanhoPopulacao = 20;
 numeroVariaveis = 1;
 numeroBits = 10;
+numeroGeracoes = 500;
 
 populacao = round(rand(tamanhoPopulacao, numeroVariaveis * numeroBits));
-
 populacaoInteiros = converterBinarioEmInteiro(populacao);
 populacaoReais = converterInteiroEmReal(populacaoInteiros, numeroBits, xMin, xMax);
+
+FX = calculaFX(populacaoReais);
+
+for i = 1:numeroGeracoes
+   novaPopulacao = variacaoBinaria(populacao);
+   novoFX = calculaFX(novaPopulacao);
+   populacao = [populaco novaPopulacao];
+   novoFX = [FX novoFX];
+end
+
+%populacaoInteiros = converterBinarioEmInteiro(populacao);
+%populacaoReais = converterInteiroEmReal(populacaoInteiros, numeroBits, xMin, xMax);
