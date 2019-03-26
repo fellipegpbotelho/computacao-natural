4a3b8d37e7084985e43fb7ef83b204f78c1606dc
Fellipe Geraldo Pereira Botelho <fellipegpbotelho@gmail.com>
2019-03-26 07:51:03 -0300

    feature(Cruzamento Binário): adiciona função que faz cruzamento binário e mutação

---
+++ cruzamentoBinario.m
@@ -0,0 +1,21 @@
+function novaPopulacao = cruzamentoBinario(populacao)
+
+    [numeroPopulacao, tamanhoBits] = size(populacao);
+    
+    novaPopulacao = zeros(numeroPopulacao, tamanhoBits);
+    
+    for i = 1:numeroPopulacao
+       % Primeiro elemento aleatório entre 1 e tamanhoBits - 1
+       populacaoCorte = randperm(tamanhoBits - 1, 1);
+       % Dois primeiros elementos aleatórios entre 1 e numeroPopulacai
+       individuos = randperm(numeroPopulacao, 2);
+       % Cocatena a parte da direita da populacao de corte do primeiros elemento com a parte da
+       % direita da populacao de corte do segundo elemento
+       populacao(i, :) = [populacao(individuos(1), 1:populacaoCorte) individuos(p(2), populacaoCorte + 1:end)];
+       % Mutação de bits
+       % Primeiro elemento aleatório entre 1 e tamanhoBits
+       r = randperm(tamanhoBits, 1);
+       % O elemento recebe 1 - ele mesmo
+       populacao(i, r) = 1 - populacao(i, r);
+    end
+end
